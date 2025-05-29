6af462cddf02af6da5bd3fb013344b4dd448bace
Agustin Alvarez Fijo <aalvarezfi@ayesa.es>
2025-05-29 13:06:42 +0200

    Import añadido

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -19,6 +19,7 @@ import './Homepage.css';
 import io from 'socket.io-client';
 import { UserContext } from '../../utils/UserContext';
 import SignIn from '../../components/auth/SignIn';
+import SignIn from '../../components/auth/SignUp';
 import {
   Heading,
   VStack,
